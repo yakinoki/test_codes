d9bcfee604b60c62fbe9cd6b7daa470f16035eae
unknown <yakinoki>
2022-05-28 22:04:04 +0900

    20220528

--- star_sun_moon.py
+++ star_sun_moon.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 card_marks = 'star,sun,moon'
 marks = card_marks.split(',')
@@ -6,10 +7,22 @@ print(marks)
 print('moonは' + str(marks.index('moon')) + '番目')
 print(marks[-2])
 
+#等確率でひとつ選ぶ。
 mark = random.choice(marks)
 print(mark)
+#等確率で２つ選ぶ。
+num_trial = 2
+mark_2 = np.random.choice(marks, num_trial)
+print(mark_2)
+#確率を指定して３つ選ぶ。
+num_trial = 3
+prob = [1/5,1/5,3/5]
+mark_3 = np.random.choice(marks, num_trial, p = prob)
+print(mark_3)
 
+#並べ替え
 marks.sort()
 print(marks)
+#starを取り除く。
 marks.remove('star')
 print(marks)
